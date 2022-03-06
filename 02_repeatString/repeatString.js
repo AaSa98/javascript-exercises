e618c4511b282df45e64e7e23df597ddd054ebef
AaSa98 <aa.sntmra@gmail.com>
2022-03-07 00:28:25 +0100

    Aufgabe 2 gelöst

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+function repeatString(word, num){
+    let repString = "";
+    if(num < 0){
+        repString = "ERROR";
+        return repString;
+    }
+    for(let i = 0; i < num; i++){
+        repString += word;
+    }
+    return repString;
 };
 
 // Do not edit below this line
