3dee1376f2799d9660d6e07e856b9aada59a55d4
AaSa98 <aa.sntmra@gmail.com>
2022-03-08 00:03:34 +0100

    aufgabe gelöst, jetzt testen

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+function sumAll(x, y){
+    let sum = 0;
+    if(x < y){
+        for(let i = x; i <= y; i++){
+            sum += i;
+        }
+    }
+    else if(y < x){
+        for(let i = y; i <= x; i++){
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
