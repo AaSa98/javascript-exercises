d2ba04f7b84e9ebd535561c1fdbaade5e9094079
AaSa98 <aa.sntmra@gmail.com>
2022-03-07 23:39:37 +0100

    ersten test von afg 04 bestanden

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+function removeFromArray(arr1, ele) {
+    for(const element of arr1){
+        if(element == ele){
+           const i = arr1.indexOf(element);
+           arr1.splice(i, 1);
+        }
+    }
+    return arr1;
 };
 
 // Do not edit below this line
