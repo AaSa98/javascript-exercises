c6b26c10a9cdfd31ce9ef1589e5cddbd7b8b5602
AaSa98 <aa.sntmra@gmail.com>
2022-03-07 23:54:49 +0100

    kommentiert

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,15 @@
 function removeFromArray(arr1) {
+    //Umwandeln der an Funktion übergebene Argumente in ein Array
     let args = [...arguments];
+    //Erstes Element des args-Arrays löschen, da dies das zu betrachtende Array ist, von dem gelöscht werden soll
     args.shift();
+    //Überprüfen aller Elemente von args
     for(const element of args){
+        //Wenn Wert aus args in Array arr1 vorkommt
         if(arr1.includes(element)){
+            //Index des Elementes in i speichern
            const i = arr1.indexOf(element);
+           //Element arr1[i] löschen
            arr1.splice(i, 1);
         }
     }
