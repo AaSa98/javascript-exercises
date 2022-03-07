12bdeb2f31703cce752dd0b97c395109df657b45
AaSa98 <aa.sntmra@gmail.com>
2022-03-08 00:09:00 +0100

    alle tests bestanden, fehlerabfragen implementiert

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
 function sumAll(x, y){
     let sum = 0;
-    if(x < y){
+    if(typeof x != "number" || typeof y != "number"){
+        return "ERROR"
+    }
+    else if(x < 0 || y < 0){
+        return "ERROR"
+    }
+    else if(x < y){
         for(let i = x; i <= y; i++){
             sum += i;
         }
