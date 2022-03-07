3e4f3e1251715d6d56d143f18d86013a922210fa
AaSa98 <aa.sntmra@gmail.com>
2022-03-07 11:06:05 +0100

    aufgabe 3 gelöst

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+function reverseString(stringNormal) {
+    let arr_stringReverse = stringNormal.split("");
+    arr_stringReverse = arr_stringReverse.reverse();
+    const stringReverse = arr_stringReverse.join("");
+    return stringReverse;
 };
 
 // Do not edit below this line
