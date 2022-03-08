d414f1ab6e9a3f46ff00ddb417fe1bfa02185f13
AaSa98 <aa.sntmra@gmail.com>
2022-03-08 15:43:14 +0100

    06 gelöst

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+function leapYears(year){
+    var isLeapYear;
+    if(year % 4 == 0 && year % 100 != 0){
+        isLeapYear = true;
+    }
+    else if(year % 400 == 0){
+        isLeapYear = true;
+    }
+    else{
+        isLeapYear = false;
+    }
+    return isLeapYear;
 
 };
 
