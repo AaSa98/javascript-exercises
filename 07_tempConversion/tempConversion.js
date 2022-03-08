9fd3f1b44ce1f0807a0e174cef13f647618ff472
AaSa98 <aa.sntmra@gmail.com>
2022-03-08 15:57:34 +0100

    07 gelöst, celsius fahreheit tested

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+function ftoc(fTemp){
+  let cTemp = (fTemp-32)*(5/9);
+  cTemp = Math.round(cTemp * 10)/10;
+  return cTemp;
 };
 
-const ctof = function() {
-
+function ctof(cTemp){
+  let fTemp = cTemp*(9/5)+32;
+  fTemp = Math.round(fTemp * 10)/10;
+  return fTemp;
 };
 
 // Do not edit below this line
