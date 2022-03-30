0ddfdf5cd1f2c2a74a60ea8c1825409705c5964d
AaSa98 <aa.sntmra@gmail.com>
2022-03-31 00:17:15 +0200

    palindromes gelöst, viel über destructive funktionen und array.reverse gelernt..

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
+function palindromes(str) {
+  let helpstr = str.toLowerCase();
+  helpstr = helpstr.replace(/\s/g, "");
+  helpstr = helpstr.replace(/[.,!?]/g, "");
+  const helparr = helpstr.split("");
+  //notwendig, weil .reverse() destructive ist und das eigentliche Array verändert
+  const helparrRev = [...helparr].reverse();
+  const str1 = helparr.join("");
+  const str2 = helparrRev.join("");
+  return str1 == str2;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
