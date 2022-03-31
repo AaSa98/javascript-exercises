0eb0bd3135a4e1a571196766daec27878dbd7810
AaSa98 <aa.sntmra@gmail.com>
2022-03-31 23:52:59 +0200

    Fibonacci durchgespielt

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
-};
+function fibonacci(nmr) {
+  //einfachste Version ist fib(1) = 1 & fib(2) = 1
+  nmr = parseInt(nmr)
+  if (nmr < 0) {
+    return "OOPS";
+  } else if (nmr == 1 || nmr == 2) {
+    return 1;
+  } else {
+    return fibonacci(nmr - 1) + fibonacci(nmr - 2);
+  }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
