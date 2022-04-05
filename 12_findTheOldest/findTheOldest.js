dffc6fdc34a9ee8b60d535a95ad434550308feec
AaSa98 <aa.sntmra@gmail.com>
2022-04-06 01:00:51 +0200

    aufgabe 12 gelöst

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
 function findTheOldest(people) {
-    peopleArr = []
-};
+  //bildet Array aus Objekt people
+  peopleArr = Array.from(people);
+  peopleArr.forEach((human) => {
+    //überprüft, ob jede Person ein Todesjahr hat, falls nicht, nimmt sie das aktuelle Jahr, weil Person ja dann so alt ist (nach dieser einfachen Rechnung)
+    if (!human.yearOfDeath) {
+      human.yearOfDeath = new Date().getFullYear();
+    }
+  });
+
+  //.sort lässt Person mit größter Differenz aus yearOfDeath-yearOfBirth an erste Stelle des Arrays wandern
+  peopleArr.sort((a, b) =>
+    a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth ? 1 : -1
+  );
+  return peopleArr[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
